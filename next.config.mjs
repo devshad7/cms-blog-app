65c2810a083428e181841ccfc941cf1375d5b7a6
Md Shad Khan <devshaad@Mac.lan>
2024-10-03 18:27:50 +0530

    feat: added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+    dest: 'public',
+    register: true,
+    disable: process.env.NODE_ENV === 'development',  // PWA disabled in development
+});
+
+// Move the 'images' configuration outside of 'withPWA'
+export default {
+    ...nextConfig,
     images: {
         domains: ['images.ctfassets.net'],
     },
 };
-
-export default nextConfig;
